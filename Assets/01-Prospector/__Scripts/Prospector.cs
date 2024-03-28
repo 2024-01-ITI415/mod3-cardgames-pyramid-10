af30cd257a1732435be4831da528a26af0a5d3ed
Hades1412 <209030wolf555@gmail.com>
2024-03-28 19:39:32 -0400

    Im so lost rn

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -10,11 +10,14 @@ public class Prospector : MonoBehaviour {
 	static public Prospector 	S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
+	public TextAsset deckXML;
+	public TextAsset layoutXML;
 
 
 	[Header("Set Dynamically")]
 	public Deck					deck;
+
+	public Layout layout;
 
 	void Awake(){
 		S = this;
@@ -25,11 +28,7 @@ public class Prospector : MonoBehaviour {
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle(ref deck.cards);
 
-		Card c;
-		for (int cNum=0; cNum<deck.cards.Count; cNum++){
-			c=deck.cards[cNum];
-			c.transform.localPosition = new Vector3((cNum%13)*3, cNum/13*4, 0);
-		}
+		layout = GetComponent<Layout> ();
+		layout.ReadLayout(layoutXML.text);
 	}
-
 }
