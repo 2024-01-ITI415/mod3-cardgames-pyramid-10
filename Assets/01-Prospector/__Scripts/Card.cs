9fe90244e37a1e8a06ba090d37947c94e9ebf8f6
MaxMicheller1 <157072693+MaxMicheller1@users.noreply.github.com>
2024-03-28 19:01:35 -0400

    Implementing Game Logic

--- Assets/01-Prospector/__Scripts/Card.cs
+++ Assets/01-Prospector/__Scripts/Card.cs
@@ -94,6 +94,11 @@ public class Card : MonoBehaviour
 		}
 	}
 
+	// Virtual methods can be overridden by subclass methods with the same name
+    virtual public void OnMouseUpAsButton() {
+        print (name); // When clicked, this outputs the card name
+    }
+
 
 	// // Use this for initialization
 	// void Start () {
